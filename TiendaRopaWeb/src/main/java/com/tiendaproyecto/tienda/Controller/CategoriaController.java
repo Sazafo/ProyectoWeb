80725b06753d0e60a2964ecd158152a30436c40c
Sazafo <sazafo1809@gmail.com>
2025-11-19 19:58:28 -0600

    commit santiago

--- TiendaRopaWeb/src/main/java/com/tiendaproyecto/tienda/Controller/CategoriaController.java
+++ TiendaRopaWeb/src/main/java/com/tiendaproyecto/tienda/Controller/CategoriaController.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.tiendaproyecto.tienda.Controller;
+package com.tiendaproyecto.tienda.controller;
 
-/**
- *
- * @author bren
- */
+import com.tiendaproyecto.tienda.domain.Categoria;
+import com.tiendaproyecto.tienda.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+@Controller
+@RequestMapping("/categorias")
 public class CategoriaController {
-    
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    // HU-016: Listar todas las categorías
+    @GetMapping
+    public String listarCategorias(Model model) {
+        List<Categoria> categorias = categoriaRepository.findAll();
+        model.addAttribute("categorias", categorias);
+        return "categorias/lista";
+    }
 }
